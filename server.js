0baca634b61ea98e43010383f47b6b5a2a8d1b7a
Vadym Demchenko <dem27va@gmail.com>
2021-05-23 22:49:52 +0300

    second commit wit node_modules again

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
+const indexRoute = require("./routes/index.js");
+
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use("/", indexRoute);
 
 app.listen(process.env.PORT || 3000);
